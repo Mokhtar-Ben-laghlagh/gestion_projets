1ac8c1426a3dc47c4770f5d2649a5b470fed4a5f
Mokhtar-Ben-laghlagh <m.benlaghlagh2658@uca.ac.ma>
2026-04-02 14:32:25 +0100

    Projet

---
+++ src/main/java/ma/fstg/gestionprojets/GestionprojetsApplication.java
@@ -0,0 +1,13 @@
+package ma.fstg.gestionprojets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionprojetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionprojetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/fstg/gestionprojets/GestionprojetsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fstg.gestionprojets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionprojetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
